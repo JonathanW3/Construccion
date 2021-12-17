ae88af0e17c1aba7f5300fc22f286b8d99390e1b
Jonathan Alcoser <95832509+JonathanW3@users.noreply.github.com>
2021-12-16 21:15:28 -0500

    v0
    Ingreso datos

---
+++ xmlserving-master/xmlserving/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name, email_address) VALUES ('Ana', 'Lopez', 'a@g.com');
+INSERT INTO employees (first_name, last_name, email_address) VALUES ('Alberto', 'Cruz', 'a@c.com');
+INSERT INTO employees (first_name, last_name, email_address) VALUES ('Julia', 'Paz', 'j@p.com');
